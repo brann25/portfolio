0b00cd9d8863067e20bbdae0252b046970ffbef2
brann25 <120200809+brann25@users.noreply.github.com>
2023-09-26 21:21:52 -0400

    Delete src/component directory

--- src/component/Page_Project3_Cinema.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Page_Project3_Cinema() {
-  return (
-    <div>Page_Project3_Cinema</div>
-  )
-}
--- src/component/Page_Project3_Illustration.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Page_Project3_Illustration() {
-  return (
-    <div>Page_Project3_Illustration</div>
-  )
-}
